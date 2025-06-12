2a0b264c6e571730fb9e17b1006fa63c4fea56ff
Ludovic ENGRAND <ludovic.engrand@sap.com>
2025-06-12 20:35:38 +0200

    handle empty route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { ConfigComponent } from './config.component';
 import { PlayerComponent } from './player.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'config' },
   { path: 'config', component: ConfigComponent },
   { path: 'player', component: PlayerComponent },
   { path: '**', redirectTo: 'config' },
