5f2f27d24aeefea9fae0b94133fd2b3a8f077f42
Ludovic ENGRAND <ludovic.engrand@sap.com>
2025-06-13 15:09:32 +0200

    initial cleanup commit

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appRouter } from './app/app.routes';
-
-bootstrapApplication(AppComponent, {
-  providers: [appRouter],
-});
